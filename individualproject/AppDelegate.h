60fe6b5f7513c6a06e31445cf443f3844fc45707
Tito Babatunde <taliax023@gmail.com>
2022-10-24 10:15:17 -0400

    Initial Commit

---
+++ individualproject/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  individualproject
+//
+//  Created by Tito Babatunde on 10/24/22.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ individualproject/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  individualproject
+//
+//  Created by Tito Babatunde on 10/24/22.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+    }
+    return NSApplicationMain(argc, argv);
+}
